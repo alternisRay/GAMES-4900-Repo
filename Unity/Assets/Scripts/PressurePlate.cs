6b6a52bde6dfb1511a10a1df4ec84f60942fae66
Ray S <rsarlatte@outlook.com>
2026-02-25 15:29:39 -0700

    PEA3 Unity Complete
    created PEA_3.unity scene
    created CoinBehavior.cs script
    created coin-gold.prefab with trigger box

--- Unity/Assets/Scripts/PressurePlate.cs
+++ Unity/Assets/Scripts/PressurePlate.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Pressure Plate Activated");
